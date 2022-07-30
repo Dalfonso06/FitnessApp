fa15163b86d1754618e40f88ad3ed814841fb832
Dalfonso06 <danielalfons06@gmail.com>
2022-07-30 15:19:13 -0400

    Initial commit

---
+++ FitnessApp/MainTabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Daniel Alfonso on 7/29/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
