437dc0223c3799ae62b56fa041fed46828633e42
Dalfonso06 <danielalfons06@gmail.com>
2022-12-22 12:18:13 -0500

    Setup notifications

--- FitnessApp/MainTabs/NotificationViewController.swift
+++ FitnessApp/MainTabs/NotificationViewController.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 
+/// Notifications View Controller
 class NotificationViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Notifications"
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
+
+extension NotificationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
 }
