41e85679f30f4f5a773889262f108e1103928d94
Dalfonso06 <danielalfons06@gmail.com>
2022-12-22 15:18:06 -0500

    Profile View Header Setup

---
+++ FitnessApp/Resources/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileInfoHeaderCollectionReusableView.swift
+//  FitnessApp
+//
+//  Created by Daniel Alfonso on 12/22/22.
+//
+
+import UIKit
+
+class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+        static let identifier = "ProfileInfoHeaderCollectionReusableView"
+}
---
+++ FitnessApp/Resources/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileTabsCollectionReusableView.swift
+//  FitnessApp
+//
+//  Created by Daniel Alfonso on 12/22/22.
+//
+
+import UIKit
+
+class ProfileTabsCollectionReusableView: UICollectionReusableView {
+        static let identifier = "ProfileTabsCollectionReusableView"
+}
